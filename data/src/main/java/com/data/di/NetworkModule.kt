cca0c3e7e7edceea1c27949f00862a2057bee614
jeyakumar.s <jeyakumar.s@optisolbusiness.com>
2022-07-04 17:07:59 +0530

    - Renamed

--- data/src/main/java/com/data/di/NetworkModule.kt
+++ data/src/main/java/com/data/di/NetworkModule.kt
@@ -24,7 +24,7 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun firebaseRepository(apiRestService: ApiRestService): RestDataSource {
+    fun restRepository(apiRestService: ApiRestService): RestDataSource {
         return RestRepository(apiRestService)
     }
 
